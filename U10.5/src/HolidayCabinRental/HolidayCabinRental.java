c9196c302e698aed13a44ff30ed38f8633020897
JOLUPEGA <53803133+JOLUPEGA@users.noreply.github.com>
2020-02-23 22:37:05 -0600

    Add files via upload

---
+++ U10.5/src/HolidayCabinRental/HolidayCabinRental.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HolidayCabinRental;
+
+import CabinRental.CabinRental;
+
+/**
+ *
+ * @author Jose Luis Perez
+ */
+public class HolidayCabinRental extends CabinRental{
+    public static final int recargo = 150;
+    
+    public HolidayCabinRental(int numCabina){
+    super(numCabina);
+    }
+    
+}
